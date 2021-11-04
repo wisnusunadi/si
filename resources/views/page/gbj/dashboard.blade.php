8e5ab44610ed7ff3a9c97f7d925267cd680b779c
ariwijaya82 <65126368+ariwijaya82@users.noreply.github.com>
2021-11-04 17:10:05 +0700

    add dashboard

--- resources/views/page/gbj/dashboard.blade.php
+++ resources/views/page/gbj/dashboard.blade.php
@@ -21,13 +21,151 @@
 
                     {{-- Produk --}}
                     <div class="tab-pane fade show active card-body" id="dashboard-produk" role="tabpanel"
-                        aria-labelledby="dashboard-produk-tab"></div>
+                        aria-labelledby="dashboard-produk-tab">
+                        <div class="row">
+                            <div class="col-sm">
+                              <div class="card">
+                                <div class="card-header bg-yellow">
+                                  <h3 class="card-title">Jumlah Stok 10 sampai 20 Unit</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                  <table class="table table-bordered">
+                                    <thead>
+                                      <tr>
+                                        <th style="width: 10px">No</th>
+                                        <th>Produk</th>
+                                        <th>Stok</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      <tr>
+                                        <td>1.</td>
+                                        <td>Ambulatory</td>
+                                        <td>15 Unit</td>
+                                      </tr>
+                                      <tr>
+                                        <td>1.</td>
+                                        <td>Ambulatory</td>
+                                        <td>15 Unit</td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer clearfix">
+                                  <ul class="pagination pagination-sm m-0 float-right">
+                                    <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+                                  </ul>
+                                </div>
+                              </div>
+                              <!-- /.card -->
+
+                              <!-- /.card -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm">
+                                <div class="card">
+                                  <div class="card-header bg-orange">
+                                    <h3 class="card-title">Jumlah Stok 9 sampai 5 Unit</h3>
+                                  </div>
+                                  <!-- /.card-header -->
+                                  <div class="card-body">
+                                    <table class="table table-bordered">
+                                      <thead>
+                                        <tr>
+                                          <th style="width: 10px">No</th>
+                                          <th>Produk</th>
+                                          <th>Stok</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        <tr>
+                                          <td>1.</td>
+                                          <td>Ambulatory</td>
+                                          <td>15 Unit</td>
+                                        </tr>
+                                        <tr>
+                                          <td>1.</td>
+                                          <td>Ambulatory</td>
+                                          <td>15 Unit</td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                  <!-- /.card-body -->
+                                  <div class="card-footer clearfix">
+                                    <ul class="pagination pagination-sm m-0 float-right">
+                                      <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                      <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                      <li class="page-item"><a class="page-link" href="#">»</a></li>
+                                    </ul>
+                                  </div>
+                                </div>
+                                <!-- /.card -->
+                                <!-- /.card -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm">
+                                <div class="card">
+                                    <div class="card-header bg-danger">
+                                      <h3 class="card-title">Jumlah Stok 1 sampai 4 Unit</h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body">
+                                      <table class="table table-bordered">
+                                        <thead>
+                                          <tr>
+                                            <th style="width: 10px">No</th>
+                                            <th>Produk</th>
+                                            <th>Stok</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <td>1.</td>
+                                            <td>Ambulatory</td>
+                                            <td>15 Unit</td>
+                                          </tr>
+                                          <tr>
+                                            <td>1.</td>
+                                            <td>Ambulatory</td>
+                                            <td>15 Unit</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                    <!-- /.card-body -->
+                                    <div class="card-footer clearfix">
+                                      <ul class="pagination pagination-sm m-0 float-right">
+                                        <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">»</a></li>
+                                      </ul>
+                                    </div>
+                                  </div>
+                                <!-- /.card -->
+                            </div>
+                          </div>
+                          
+                          
+                    </div>
                         
 
                     {{-- Transfer --}}
                     <div class="tab-pane fade show active card-body" id="dashboard-transfer" role="tabpanel"
-                        aria-labelledby="dashboard-transfer-tab"></div>
-                    <div class="row"></div>
+                        aria-labelledby="dashboard-transfer-tab">
+                        <div class="row"></div>
+                    </div>
+
                 </div>
             </div>
         </div>
