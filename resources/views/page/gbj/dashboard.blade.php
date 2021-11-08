6ad7ee36065ab57b0db83d130f3895c0cb8c38cc
ariwijaya82 <65126368+ariwijaya82@users.noreply.github.com>
2021-11-08 11:07:16 +0700

    revision all

--- resources/views/page/gbj/dashboard.blade.php
+++ resources/views/page/gbj/dashboard.blade.php
@@ -30,38 +30,9 @@
                                 </div>
                                 <!-- /.card-header -->
                                 <div class="card-body">
-                                  <table class="table table-bordered">
-                                    <thead>
-                                      <tr>
-                                        <th style="width: 10px">No</th>
-                                        <th>Produk</th>
-                                        <th>Stok</th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <td>1.</td>
-                                        <td>Ambulatory</td>
-                                        <td>15 Unit</td>
-                                      </tr>
-                                      <tr>
-                                        <td>1.</td>
-                                        <td>Ambulatory</td>
-                                        <td>15 Unit</td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
+                                  <canvas id="myChart" width="400" height="400"></canvas>
                                 </div>
                                 <!-- /.card-body -->
-                                <div class="card-footer clearfix">
-                                  <ul class="pagination pagination-sm m-0 float-right">
-                                    <li class="page-item"><a class="page-link" href="#">«</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">»</a></li>
-                                  </ul>
-                                </div>
                               </div>
                               <!-- /.card -->
 
@@ -175,6 +146,42 @@
 
 @section('adminlte_js')
 <script>
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 </script>
 @stop
