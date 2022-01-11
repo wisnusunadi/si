2549fd79e7c6809a92b85b213b78ad476a9bb7d2
Ari Wijaya <ariwijaya.its@gmail.com>
2022-01-11 07:46:54 +0700

    add header to import file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,8 @@
-window.axios = require("axios");
 window.$ = window.jQuery = require("jquery");
+window.OverlayScrollbars = require("overlayscrollbars");
+
 import "bootstrap/dist/js/bootstrap.bundle";
 import "datatables.net-bs4/js/dataTables.bootstrap4.min";
 import "chart.js/dist/chart";
 import "select2/dist/js/select2.full";
 import "fullcalendar";
-window.OverlayScrollbars = require("overlayscrollbars");
