4b83102d120415f93ef809f611972d147b9de750
dhimaseka <dimaseka83@gmail.com>
2022-01-26 16:03:51 +0700

    push

--- resources/views/page/produksi/pengiriman.blade.php
+++ resources/views/page/produksi/pengiriman.blade.php
@@ -217,6 +217,30 @@
         </div>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Edit No Seri</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                  <label for="">No Seri</label>
+                  <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
 
 @section('adminlte_js')
