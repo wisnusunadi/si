c767fb4a8965ca5aa522fdeb6da176ae6aa3abbf
aadzhaan <adzhanilah@gmaiil.com>
2022-03-25 15:47:30 +0700

    minor

--- resources/views/page/logistik/laporan/LaporanLogistikEx.blade.php
+++ resources/views/page/logistik/laporan/LaporanLogistikEx.blade.php
@@ -129,7 +129,7 @@
                 <td rowspan="{{$csolog}}">{{$add}}</td>
                 <td rowspan="{{$csolog}}">{{$prov}}</td>
                 <td rowspan="{{$rowspan}}">{{$i->nosurat}}</td>
-                <td rowspan="{{$rowspan}}">{{$i->tgl_kirim}}</td>
+                <td rowspan="{{$rowspan}}">{{date('d-m-Y', strtotime($i->tgl_kirim))}}</td>
                 <td rowspan="{{$rowspan}}">{{$i->noresi}}</td>
                 <td rowspan="{{$rowspan}}">
                 @if($i->ekspedisi_id)
@@ -220,7 +220,7 @@
         @else
             <tr>
             <td rowspan="{{$rowspan}}">{{$i->nosurat}}</td>
-            <td rowspan="{{$rowspan}}">{{$i->tgl_kirim}}</td>
+            <td rowspan="{{$rowspan}}">{{date('d-m-Y', strtotime($i->tgl_kirim))}}</td>
             <td rowspan="{{$rowspan}}">{{$i->noresi}}</td>
             <td rowspan="{{$rowspan}}">
                 @if($i->ekspedisi_id)
@@ -304,5 +304,128 @@
         @endif
         @endforeach
     </tbody>
-
+        {{-- <tbody>
+        @foreach ($data as $d)
+        <tr>
+            <td style="text-align:left">{{$loop->iteration}}</td>
+            <td style="text-align:left">
+                @if(isset($d->DetailPesananProduk))
+                {{$d->DetailPesananProduk->DetailPesanan->Pesanan->so}}
+                @else
+                {{$d->DetailPesananPart->Pesanan->so}}
+                @endif
+            </td>
+            <td style="text-align:left">
+                @if(isset($d->DetailPesananProduk))
+                {{$d->DetailPesananProduk->DetailPesanan->Pesanan->no_po}}
+                @else
+                {{$d->DetailPesananPart->Pesanan->no_po}}
+                @endif
+            </td>
+            <td style="text-align:left">{{ $d->Logistik->nosurat}}</td>
+            <td style="text-align:left">{{ $d->Logistik->tgl_kirim}}</td>
+            <td style="text-align:left">
+                @if($d->Logistik->noresi == "")
+                -
+                @else
+                {{$d->Logistik->noresi}}
+                @endif
+            </td>
+            <td style="text-align:left">
+                <?php
+                // if (isset($d->DetailPesananProduk)) {
+                //     $name = explode('/', $d->DetailPesananProduk->DetailPesanan->pesanan->so);
+                //     if ($name[1] == 'EKAT') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Ekatalog->instansi;
+                //     } else if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spa->Customer->nama;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spb->Customer->nama;
+                //     }
+                // } else {
+                //     $name = explode('/', $d->DetailPesananPart->Pesanan->so);
+                //     if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananPart->Pesanan->Spa->Customer->nama;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananPart->Pesanan->Spb->Customer->nama;
+                //     }
+                // }
+                ?>
+            </td>
+            <td style="text-align:left">
+                <?php
+                // if (isset($d->DetailPesananProduk)) {
+                //     $name = explode('/', $d->DetailPesananProduk->DetailPesanan->pesanan->so);
+                //     if ($name[1] == 'EKAT') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Ekatalog->instansi;
+                //     } else if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spa->Customer->alamat;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spb->Customer->alamat;
+                //     }
+                // } else {
+                //     $name = explode('/', $d->DetailPesananPart->Pesanan->so);
+                //     if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananPart->Pesanan->Spa->Customer->alamat;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananPart->Pesanan->Spb->Customer->alamat;
+                //     }
+                // }
+                ?>
+            </td>
+            <td style="text-align:left">
+                <?php
+                // if (isset($d->DetailPesananProduk)) {
+                //     $name = explode('/', $d->DetailPesananProduk->DetailPesanan->pesanan->so);
+                //     if ($name[1] == 'EKAT') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Ekatalog->Provinsi->nama;
+                //     } else if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spa->Customer->Provinsi->nama;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananProduk->DetailPesanan->Pesanan->Spb->Customer->Provinsi->nama;
+                //     }
+                // } else {
+                //     $name = explode('/', $d->DetailPesananPart->Pesanan->so);
+                //     if ($name[1] == 'SPA') {
+                //         echo $d->DetailPesananPart->Pesanan->Spa->Customer->Provinsi->nama;
+                //     } else if ($name[1] == 'SPB') {
+                //         echo $d->DetailPesananPart->Pesanan->Spb->Customer->Provinsi->nama;
+                //     }
+                // }
+                ?>
+            </td>
+            <td style="text-align:left">
+                @if (!empty($d->Logistik->ekspedisi_id))
+                {{ $d->Logistik->Ekspedisi->nama}}
+                @else
+                {{ $d->Logistik->nama_pengirim}}
+                @endif
+            </td>
+            <td style="text-align:left">
+                @if (isset($d->DetailPesananProduk))
+                {{$d->DetailPesananProduk->GudangBarangJadi->Produk->nama}}
+                @else
+                {{ $d->DetailPesananPart->Sparepart->nama}}
+                @endif
+            </td>
+            <td style="text-align:left">
+                @if (isset($d->DetailPesananProduk))
+                @if ($d->DetailPesananProduk->GudangBarangJadi->nama != '')
+                {{$d->DetailPesananProduk->GudangBarangJadi->nama}}
+                @endif
+                @endif
+            </td>
+            <td style="text-align:left">
+                @if (isset($d->NoseriDetailLogistik))
+                {{ $d->NoseriDetailLogistik->count()}}
+                @else
+                {{ $d->DetailPesananPart->jumlah;}}
+                @endif
+            </td>
+            <td style="text-align:left">
+                {{ $d->Logistik->State->nama}}
+            </td>
+        </tr>
+        @endforeach
+    </tbody> --}}
 </table>
