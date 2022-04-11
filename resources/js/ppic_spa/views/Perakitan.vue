b8977f6ccb9eebaab881dceac930ea9c069ef8b0
dhimaseka <dimaseka83@gmail.com>
2022-04-11 13:49:13 +0700

    ++

--- resources/js/ppic_spa/views/Perakitan.vue
+++ resources/js/ppic_spa/views/Perakitan.vue
@@ -44,8 +44,8 @@
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
-                    <p class="modal-card-title">{{this.nama_produk}}</p>
-                    <button class="delete" aria-label="close"></button>
+                    <p class="modal-card-title" v-html="this.nama_produk"></p>
+                    <button class="delete" aria-label="close" @click="showModal = false"></button>
                 </header>
                 <section class="modal-card-body">
                     <div v-if="this.loading">Loading...</div>
