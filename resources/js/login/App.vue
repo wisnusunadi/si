d6f4ff9884ac7f733a5394540de1c68539c1f3f2
dhimaseka <dimaseka83@gmail.com>
2022-09-01 10:43:38 +0700

    ++

---
+++ resources/js/login/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    methods: {
+        async login(){
+            let data = {
+                username: "superuser.api",
+                password: "password"
+            }
+            await axios.post('https://sinko.api.hyperdatasystem.com/api/login', data).then(response => {
+                sessionStorage.setItem('token', response.data.token);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    mounted() {
+        this.login();
+    }
+}
+</script>
