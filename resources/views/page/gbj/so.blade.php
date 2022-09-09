b1efbeb5d9accb393d88b9efd2359802fef881e6
yogiaditya <adityayogi711@gmail.com>
2022-09-09 09:23:12 +0700

    fix api direksi

--- resources/views/page/gbj/so.blade.php
+++ resources/views/page/gbj/so.blade.php
@@ -454,6 +454,22 @@
 @section('adminlte_js')
 <script>
     var access_token = localStorage.getItem('lokal_token');
+    if (access_token == null) {
+            Swal.fire({
+                title: 'Session Expired',
+                text: 'Silahkan login kembali',
+                icon: 'warning',
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'OK'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    event.preventDefault();
+                    document.getElementById('logout-form').submit();
+                }
+            })
+        }
     $(document).ready(function () {
         let auth = $('#auth').val();
         $('#head-cb-so').prop('checked', false);
