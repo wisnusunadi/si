87c5bfa5099b80a70ee2e78950f274179f1de3c7
wisnusunadi <barto7773@gmail.com>
2023-01-23 09:05:30 +0700

    isaac

--- config/database.php
+++ config/database.php
@@ -123,6 +123,26 @@ return [
             ]) : [],
         ],
 
+        'erp_kalibrasi' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_6', '192.168.13.2'),
+            'port' => env('DB_PORT_6', '3306'),
+            'database' => env('DB_DATABASE_6', 'forge'),
+            'username' => env('DB_USERNAME_6', 'forge'),
+            'password' => env('DB_PASSWORD_6', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
