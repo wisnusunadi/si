97214a33de2b2b6672552ddc8126591b91bbf17f
dhimaseka <dimaseka83@gmail.com>
2023-08-09 15:34:01 +0700

    ++

--- resources/views/page/logistik/so/modalautocheckbox.blade.php
+++ resources/views/page/logistik/so/modalautocheckbox.blade.php
@@ -82,8 +82,8 @@
     }
 
     $(document).on('click', '#simpandata', function () {
-        // split nomor seri berdasarkan enter atau koma atau tab
-        let nomor_seri = $('#nomor_seri').val().split(/[\n,|\t]+/g);
+        // split nomor seri berdasarkan enter atau koma atau tab atau spasi
+        let nomor_seri = $('#nomor_seri').val().split(/[\n, \t]/);
         // remove spasi tiap nomor seri
         nomor_seri = nomor_seri.map(function (item) {
             return item.trim();
