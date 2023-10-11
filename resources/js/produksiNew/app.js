0a441e7e362ea3421aa5ded18962784e445e948b
your_username <dimaseka83@gmail.com>
2023-10-11 13:14:53 +0700

    ++

---
+++ resources/js/produksiNew/app.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Index from "./Index.vue";
+import router from "./router";
+import VueRouter from "vue-router";
+import VueSweetalert2 from "vue-sweetalert2";
+import Vuex from "vuex";
+import vSelect from "vue-select";
+import storeData from "./store";
+import "vue-select/dist/vue-select.css";
+
+window.Vue = Vue;
+Vue.use(VueRouter);
+Vue.use(VueSweetalert2);
+Vue.use(Vuex);
+Vue.component("v-select", vSelect);
+
+const store = new Vuex.Store(storeData);
+
+const app = new Vue({
+    el: "#app",
+    router,
+    store,
+    components: {
+        index: Index
+    }
+});
