576e69e5efef794757ad1f0ef6b358d1d1296d69
your_username <dimaseka83@gmail.com>
2023-10-15 13:04:53 +0700

    ++

--- resources/js/gbj/page/penerimaanRework/riwayat/modalProduk/index.vue
+++ resources/js/gbj/page/penerimaanRework/riwayat/modalProduk/index.vue
@@ -102,7 +102,7 @@ export default {
                                         <label for="">Tanggal Transfer</label>
                                         <div class="card nomor-po">
                                             <div class="card-body">
-                                                <span id="po">{{ dateFormat(dataSelected.tgl_transfer) }}</span>
+                                                <span id="po">{{ dateFormat(dataSelected.tgl_tf) }}</span>
                                             </div>
                                         </div>
                                     </div>
