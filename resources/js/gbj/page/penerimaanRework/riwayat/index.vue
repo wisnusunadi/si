7f6a085844c2e4b7dba28c4123fe15ae78f08a6a
your_username <dimaseka83@gmail.com>
2023-10-20 09:17:18 +0700

    ++

--- resources/js/gbj/page/penerimaanRework/riwayat/index.vue
+++ resources/js/gbj/page/penerimaanRework/riwayat/index.vue
@@ -1,44 +1,53 @@
 <script>
-import axios from 'axios';
-import Pagination from '../../../components/pagination.vue';
-import Table from './table.vue';
+import modalProduk from './modalProduk'
+import DataTable from '../../../components/DataTable.vue'
 export default {
     components: {
-        Pagination,
-        Table,
+        modalProduk,
+        DataTable,
     },
-    props: ['dataTable'],
     data() {
         return {
-            search: '',
-            renderPaginate: [
+            dataSelected: {},
+            showModal: false,
+            headers: [
+                { text: 'No Urut', value: 'no_urut' },
+                { text: 'Nama Produk', value: 'nama' },
+                { text: 'Jumlah Transfer', value: 'jumlah' },
+                { text: 'Tanggal Penerimaan', value: 'tgl_tf' },
+                { text: 'Aksi', value: 'aksi', sortable: false },
             ],
+            search: '',
         }
     },
+    props: ['dataTable'],
     methods: {
-        updateFilteredDalamProses(data) {
-            this.renderPaginate = data;
-        },
-    },
-    computed: {
-        filteredDalamProses() {
-            return this.dataTable.filter((data) => {
-                return Object.keys(data).some((key) => {
-                    return String(data[key]).toLowerCase().includes(this.search.toLowerCase());
-                });
-            });
+        detail(data) {
+            this.dataSelected = JSON.parse(JSON.stringify(data))
+
+            this.showModal = true
+            this.$nextTick(() => {
+                $('.modalProduk').modal('show')
+            })
         },
     },
 }
 </script>
 <template>
     <div v-if="!$store.state.loading">
+        <modalProduk v-if="showModal" @closeModal="showModal = false" :dataSelected="dataSelected" />
         <div class="d-flex flex-row-reverse bd-highlight">
             <div class="p-2 bd-highlight">
                 <input type="text" v-model="search" class="form-control" placeholder="Cari...">
             </div>
         </div>
-        <Table :dataTable="renderPaginate"></Table>
-        <pagination :filteredDalamProses="filteredDalamProses" @updateFilteredDalamProses="updateFilteredDalamProses" />
+        <DataTable :headers="headers" :items="dataTable" :search="search">
+            <template #item.aksi="{ item }">
+                <button class="btn btn-sm btn-outline-info" @click="detail(item)">
+                    <i class="fas fa-info-circle"></i>
+                    Detail
+                </button>
+            </template>
+        </DataTable>
     </div>
 </template>
