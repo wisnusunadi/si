950a954f368c9d2e062c48efc57fb6a5df48d21a
your_username <dimaseka83@gmail.com>
2023-10-20 10:47:35 +0700

    ++

--- resources/views/page/produksi/printreworks/cetakpermintaanbarangjadi.blade.php
+++ resources/views/page/produksi/printreworks/cetakpermintaanbarangjadi.blade.php
@@ -26,8 +26,14 @@
             max-width: 50%;
         }
 
-        table {
+        header table,
+        main table
+        {
             width: 100%;
+        }
+
+        header table tr td {
+            padding: 5px;
         }
 
         .td-full-border {
@@ -38,7 +44,37 @@
             border-left: 1px solid black;
             border-right: 1px solid black;
             border-bottom: 1px solid black;
-            padding-top: -10px;
+        }
+
+        /* full border table */
+        main table {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+
+        main table tr th, main table tr td {
+            border: 1px solid black;
+            padding: 5px;
+        }
+
+        footer {
+            position: fixed;
+            bottom: 0;
+        }
+
+        .catatan-table {
+            border: 1px solid black;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        .catatan-table tr td {
+            padding: 5px;
+        }
+
+        .table-dibuat {
+            width: 20%;
+            margin-top: 20px;
         }
     </style>
 </head>
@@ -46,37 +82,85 @@
 <body>
     <h1 class="judul">PERMINTAAN BARANG JADI</h1>
 
-    <table>
-        <tr>
-            <td style="width: 22%">No. Permintaan</td>
-            <td style="width: 1%">:</td>
-            <td style="width: 22%">0001/X/2003</td>
-            <td style="width: 25%"></td>
-            <td class="text-center td-full-border">No. Referensi</td>
-        </tr>
-        <tr>
-            <td>Tanggal Permintaan</td>
-            <td>:</td>
-            <td>0001/X/2003</td>
-            <td style="width: 25%"></td>
-            <td class="text-center td-urutan"><b>PRD-1</b></td>
-        </tr>
-        <tr>
-            <td>Tanggal Digunakan</td>
-            <td>:</td>
-            <td>0001/X/2003</td>
-        </tr>
-        <tr>
-            <td>Bagian</td>
-            <td>:</td>
-            <td>0001/X/2003</td>
-        </tr>
-        <tr>
-            <td>Kegunaan untuk</td>
-            <td>:</td>
-            <td>0001/X/2003</td>
-        </tr>
-    </table>
+    <header>
+        <table>
+            <tr>
+                <td style="width: 22%">No. Permintaan</td>
+                <td style="width: 1%">:</td>
+                <td style="width: 40%">0001/X/2003</td>
+                <td style="width: 15%"></td>
+                <td class="text-center td-full-border">No. Referensi</td>
+            </tr>
+            <tr>
+                <td>Tanggal Permintaan</td>
+                <td>:</td>
+                <td>18 Oktober 2023</td>
+                <td style="width: 15%"></td>
+                <td class="text-center td-urutan"><b>PRD-1</b></td>
+            </tr>
+            <tr>
+                <td>Tanggal Digunakan</td>
+                <td>:</td>
+                <td>18 Oktober 2023</td>
+            </tr>
+            <tr>
+                <td>Bagian</td>
+                <td>:</td>
+                <td>Produksi</td>
+            </tr>
+            <tr>
+                <td>Kegunaan untuk</td>
+                <td>:</td>
+                <td>Rework Produksi Antropometri Kit 10</td>
+            </tr>
+        </table>
+    </header>
+
+    <main>
+        <table class="text-center">
+            <thead>
+                <tr>
+                    <th>Kode Barang</th>
+                    <th>Nama Barang</th>
+                    <th>Jumlah</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>656356</td>
+                    <td>Antropometri Kit 10</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>656356</td>
+                    <td>Antropometri Kit 10</td>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+
+    <footer>
+        <table class="catatan-table">
+            <tr>
+                <td>Catatan :</td>
+            </tr>
+            <tr>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nesciunt ex ratione hic iste adipisci minus. Eligendi, repudiandae, nemo suscipit consectetur deleniti nobis esse doloribus eos distinctio cupiditate at. Ab.</td>
+            </tr>
+        </table>
+
+        <table class="text-center table-dibuat">
+            <tr>
+                <td style="width: 60%">Dibuat Oleh,</td>
+            </tr>
+            <tr>
+                <td style="padding-top: 50px;">
+                    <hr>
+                </td>
+            </tr>
+        </table>
+    </footer>
 </body>
 
 </html>
