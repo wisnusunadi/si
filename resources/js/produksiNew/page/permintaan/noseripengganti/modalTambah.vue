5703d7395134224ca6f223bfa8555b7e1423af1e
your_username <dimaseka83@gmail.com>
2023-10-27 16:58:42 +0700

    ++

--- resources/js/produksiNew/page/permintaan/noseripengganti/modalTambah.vue
+++ resources/js/produksiNew/page/permintaan/noseripengganti/modalTambah.vue
@@ -1,9 +1,11 @@
 <script>
 import axios from 'axios'
 import DataTable from '../../../components/DataTable.vue'
+import seriviatext from '../../prosesSet/proses/modalTransfer/seriviatext.vue'
 export default {
     components: {
         DataTable,
+        seriviatext,
     },
     data() {
         return {
@@ -42,6 +44,7 @@ export default {
             offset: 0,
             limit: 10,
             keterangan: '',
+            showTextSeri: false,
         }
     },
     methods: {
@@ -100,6 +103,39 @@ export default {
             }
 
             this.loading = true
+        },
+        showSeriViaText() {
+            this.showTextSeri = true
+            $('.modalTambah').modal('hide')
+            this.$nextTick(() => {
+                $('.modalChecked').modal('show')
+            })
+        },
+        closeTextSeri() {
+            this.showTextSeri = false
+            this.$nextTick(() => {
+                $('.modalTambah').modal('show')
+            })
+        },
+        submit(noseri) {
+            let noserinotfound = []
+
+            let noseriarray = noseri.split(/[\n, \t]/)
+
+            for (let i = 0; noseriarray.length; i++) {
+                let found = false
+                for (let j = 0; j < this.noseri.length; j++) {
+                    if (noseriarray[i] == this.noseri[j].noseri) {
+                        if (!this.formNoseri.some(item => item.noseri === this.noseri[j].noseri)) {
+                            this.formNoseri.push({
+                                noseri: this.noseri[j].noseri,
+                            })
+                        }
+                        found = true
+                        break
+                    }
+                }
+            }
         }
     },
     mounted() {
@@ -118,112 +154,132 @@ export default {
             }
         },
         filteredNoSeri() {
-            return this.noSeriSelect.filter((item, idx) =>
-                Object.values(item).some(
-                    (val) =>
-                        val != null &&
-                        val.toString().toLowerCase().includes(this.search.toLowerCase())
-                )
-            );
+            if (this.noseri.length > 0) {
+                return this.noSeriSelect.filter((item, idx) =>
+                    Object.values(item).some(
+                        (val) =>
+                            val != null &&
+                            val.toString().toLowerCase().includes(this.search.toLowerCase())
+                    )
+                );
+            }
         },
         paginated() {
-            return this.filteredNoSeri.slice(
-                this.offset,
-                this.limit + this.offset
-            );
+            if (this.noseri.length > 0) {
+                return this.filteredNoSeri.slice(
+                    this.offset,
+                    this.limit + this.offset
+                );
+            }
         },
         hasNextPage() {
-            const nextOffset = this.offset + this.limit;
-            return Boolean(
-                this.filteredNoSeri.slice(nextOffset, this.limit + nextOffset)
-                    .length
-            );
+            if (this.noseri.length > 0) {
+                const nextOffset = this.offset + this.limit;
+                return Boolean(
+                    this.filteredNoSeri.slice(nextOffset, this.limit + nextOffset)
+                        .length
+                );
+            }
         },
         hasPrevPage() {
-            const prevOffset = this.offset - this.limit;
-            return Boolean(
-                this.filteredNoSeri.slice(prevOffset, this.limit + prevOffset)
-                    .length
-            );
+            if (this.noseri.length > 0) {
+                const prevOffset = this.offset - this.limit;
+                return Boolean(
+                    this.filteredNoSeri.slice(prevOffset, this.limit + prevOffset)
+                        .length
+                );
+            }
         },
     }
 }
 </script>
 <template>
-    <div class="modal fade modalTambah" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
-        aria-labelledby="modelTitleId" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-scrollable" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Form Penggantian Nomor Seri</h5>
-                    <button type="button" class="close" @click="closeModal">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="d-flex justify-content-end">
-                        <button class="btn btn-primary mb-1" @click="formNoseri.push({ noseri: null })">
-                            <i class="fas fa-plus"></i>
-                            Tambah
+    <div>
+        <seriviatext v-if="showTextSeri" @close="closeTextSeri" @submit="submit" />
+        <div class="modal fade modalTambah" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
+            aria-labelledby="modelTitleId" aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Form Penggantian Nomor Seri</h5>
+                        <button type="button" class="close" @click="closeModal">
+                            <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="spinner-border" role="status" v-if="loadingData">
-                        <span class="sr-only">Loading...</span>
+                    <div class="modal-body">
+                        <div class="d-flex bd-highlight">
+                            <div class="p-2 flex-grow-1 bd-highlight">
+                                <button class="btn btn-primary" @click="showSeriViaText">
+                                    Pilih No Seri Via Text
+                                </button>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <button class="btn btn-primary mb-1" @click="formNoseri.push({ noseri: null })">
+                                    <i class="fas fa-plus"></i>
+                                    Tambah
+                                </button>
+                            </div>
+                        </div>
+                        <div class="spinner-border" role="status" v-if="loadingData">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="scrollable" v-else>
+                            <table class="table text-center">
+                                <thead>
+                                    <tr>
+                                        <th>No Seri</th>
+                                        <th>Nama Produk</th>
+                                        <th>Varian</th>
+                                        <th>No Produk</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(item, index) in formNoseri" :key="index">
+                                        <td>
+                                            <v-select :options="paginated" v-model="item.noseri" @search="onSearch">
+                                                <li slot="list-footer" class="pagination">
+                                                    <button type="button" class="btn btn-secondary" :disabled="!hasPrevPage"
+                                                        @click="offset -= limit">
+                                                        Prev
+                                                    </button>
+                                                    <button type="button" class="btn btn-primary" :disabled="!hasNextPage"
+                                                        @click="offset += limit">
+                                                        Next
+                                                    </button>
+                                                </li>
+                                            </v-select>
+                                            <small class="text-danger" v-if="notifikasiSeri(item.noseri)">{{
+                                                notifikasiSeri(item.noseri) }}</small>
+                                        </td>
+                                        <td>
+                                            {{ item.noseri?.nama ?? '-' }}
+                                        </td>
+                                        <td>
+                                            {{ item.noseri?.varian ?? '-' }}
+                                        </td>
+                                        <td>
+                                            {{ item.noseri?.no_produk ?? '-' }}
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-danger btn-sm" @click="formNoseri.splice(index, 1)">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Keterangan</label>
+                            <textarea cols="5" class="form-control" v-model="keterangan"></textarea>
+                        </div>
                     </div>
-                    <div class="scrollable" v-else>
-                        <table class="table text-center">
-                            <thead>
-                                <tr>
-                                    <th>No Seri</th>
-                                    <th>Nama Produk</th>
-                                    <th>Varian</th>
-                                    <th>No Produk</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="(item, index) in formNoseri" :key="index">
-                                    <td>
-                                        <v-select :options="paginated" v-model="item.noseri" @search="onSearch">
-                                            <li slot="list-footer" class="pagination">
-                                                <button type="button" class="btn btn-secondary" :disabled="!hasPrevPage"
-                                                    @click="offset -= limit">
-                                                    Prev
-                                                </button>
-                                                <button type="button" class="btn btn-primary" :disabled="!hasNextPage"
-                                                    @click="offset += limit">
-                                                    Next
-                                                </button>
-                                            </li>
-                                        </v-select>
-                                        <small class="text-danger" v-if="notifikasiSeri(item.noseri)">{{ notifikasiSeri(item.noseri) }}</small>
-                                    </td>
-                                    <td>
-                                        {{ item.noseri?.nama ?? '-' }}
-                                    </td>
-                                    <td>
-                                        {{ item.noseri?.varian ?? '-' }}
-                                    </td>
-                                    <td>
-                                        {{ item.noseri?.no_produk ?? '-' }}
-                                    </td>
-                                    <td>
-                                        <button class="btn btn-danger btn-sm" @click="formNoseri.splice(index, 1)">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" @click="closeModal">Keluar</button>
+                        <button type="button" class="btn btn-success" :disabled="loading"
+                            @click="transfer">Transfer</button>
                     </div>
-                    <div class="form-group">
-                      <label for="">Keterangan</label>
-                      <textarea cols="5" class="form-control" v-model="keterangan"></textarea>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" @click="closeModal">Keluar</button>
-                    <button type="button" class="btn btn-success" :disabled="loading" @click="transfer">Transfer</button>
                 </div>
             </div>
         </div>
