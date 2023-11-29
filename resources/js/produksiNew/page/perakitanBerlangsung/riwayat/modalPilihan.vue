d9e676e9275ef2f36ffc8f4d210f704615568837
your_username <dimaseka83@gmail.com>
2023-11-29 08:10:46 +0700

    ++

--- resources/js/produksiNew/page/perakitanBerlangsung/riwayat/modalPilihan.vue
+++ resources/js/produksiNew/page/perakitanBerlangsung/riwayat/modalPilihan.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
     props: ['data'],
+    data() {
+        return {
+            alasan: '',
+        }
+    },
     methods: {
         closeModal() {
             $('.modalPilihan').modal('hide');
@@ -11,28 +16,37 @@ export default {
         small() {
             let cetak = JSON.stringify(this.data);
             window.open(`/produksiReworks/cetak_seri_fg_small?data=${cetak}`, '_blank')
+            this.alasan = '';
         },
         medium() {
             let cetak = JSON.stringify(this.data);
             window.open(`/produksiReworks/cetak_seri_fg_medium?data=${cetak}`, '_blank')
+            this.alasan = '';
         }
     },
 }
 </script>
 <template>
-    <div class="modal fade modalPilihan" id="modelId" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal fade modalPilihan" id="modelId" data-backdrop="static" data-keyboard="false" tabindex="-1"
+        role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
         <div class="modal-dialog modal-md modal-dialog-scrollable" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Pilihan Cetak</h5>
-                        <button type="button" class="close" @click="closeModal">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                    <button type="button" class="close" @click="closeModal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="modal-body text-center">
-                    <h1>Silahkan Pilih Hasil Cetak</h1>
-                    <button type="button" class="btn btn-primary btn-lg" @click="small">Kertas Kecil</button>
-                    <button type="button" class="btn btn-primary btn-lg" @click="medium">Kertas Medium</button>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Alasan Cetak</label>
+                        <textarea class="form-control" name="" id="" rows="3" v-model="alasan"></textarea>
+                    </div>
+                    <div class="text-center">
+                        <h1>Silahkan Pilih Hasil Cetak</h1>
+                        <button type="button" class="btn btn-primary btn-lg" @click="small">Kertas Kecil</button>
+                        <button type="button" class="btn btn-primary btn-lg" @click="medium">Kertas Medium</button>
+                    </div>
                 </div>
             </div>
         </div>
