e6f1f66acdfd45e7acb4504d8ba3074741170e52
your_username <dimaseka83@gmail.com>
2024-01-03 09:08:37 +0700

    disable pemetian logistik

--- resources/js/logistik/page/setPemetian/index.vue
+++ resources/js/logistik/page/setPemetian/index.vue
@@ -34,7 +34,7 @@ export default {
             try {
                 this.$store.dispatch('setLoading', true)
                 const { data } = await axios.get('/api/logistik/rw/')
-                const { data:kardus } = await axios.get('/api/logistik/rw/pack/show')
+                const { data: kardus } = await axios.get('/api/logistik/rw/pack/show')
                 this.proses = data.map(item => {
                     return {
                         ...item,
@@ -60,26 +60,7 @@ export default {
         <Header :title="title" :breadcumbs="breadcumbs" />
         <div class="card">
             <div class="card-body">
-                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home"
-                            type="button" role="tab" aria-controls="pills-home" aria-selected="true">Pemetian</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile"
-                            type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
-                            Pengkardusan
-                        </a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="pills-tabContent" v-if="!$store.state.loading">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                        <proses :dataTable="proses" />
-                    </div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                        <kardus :dataTable="kardus" />
-                    </div>
-                </div>
+                <kardus :dataTable="kardus" v-if="!$store.state.loading" />
                 <div class="spinner-border" role="status" v-else>
                     <span class="sr-only">Loading...</span>
                 </div>
