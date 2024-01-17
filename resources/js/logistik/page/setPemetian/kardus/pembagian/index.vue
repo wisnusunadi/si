3465cd5b375fc49880f9463b37210440b7057ac4
your_username <dimaseka83@gmail.com>
2024-01-17 14:38:03 +0700

    ++

--- resources/js/logistik/page/setPemetian/kardus/pembagian/index.vue
+++ resources/js/logistik/page/setPemetian/kardus/pembagian/index.vue
@@ -93,11 +93,11 @@ export default {
                         <a class="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home"
                             type="button" role="tab" aria-controls="pills-home" aria-selected="true">Pembagian Wilayah</a>
                     </li>
-                    <li class="nav-item" role="presentation">
+                    <!-- <li class="nav-item" role="presentation">
                         <a class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile"
                             type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Tanpa Pembagian
                             Wilayah</a>
-                    </li>
+                    </li> -->
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
