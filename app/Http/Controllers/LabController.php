bd522640fa6a2b3a42e426451d92c35e9e5cede1
wisnusunadi <barto7773@gmail.com>
2024-02-07 15:16:35 +0700

    riwayat lab

--- app/Http/Controllers/LabController.php
+++ app/Http/Controllers/LabController.php
@@ -34,27 +34,122 @@ class LabController extends Controller
     //
     public function riwayat_lab_uji(Request $request)
     {
-        $years = $request->years;
-        $data = SystemLog::where(['tipe' => 'Lab', 'subjek' => 'Kalibrasi Produk'])->whereYear('created_at', $years)->get();
+        $data = UjiLabDetail::select(
+            'jenis_pemilik.nama as jp',
+            'pesanan.no_po',
+            'uji_lab.pesanan_id as p_id',
+            'karyawans.nama as pemeriksa',
+            'uji_lab.no_order',
+            'uji_lab.nama',
+            'uji_lab.alamat',
+            'uji_lab_detail.no',
+            'uji_lab_detail.no_sertifikat',
+            'uji_lab_detail.tgl_masuk',
+            'metode_lab.metode',
+            'produk.nama as produk',
+            'noseri_barang_jadi.noseri',
+            'uji_lab_detail.tgl_kalibrasi'
+        )
+            ->leftJoin('uji_lab', 'uji_lab.id', '=', 'uji_lab_detail.uji_lab_id')
+            ->leftJoin('detail_pesanan_produk', 'detail_pesanan_produk.id', '=', 'uji_lab_detail.detail_pesanan_produk_id')
+            ->leftJoin('gdg_barang_jadi', 'gdg_barang_jadi.id', '=', 'detail_pesanan_produk.gudang_barang_jadi_id')
+            ->leftJoin('produk', 'produk.id', '=', 'gdg_barang_jadi.produk_id')
+            ->leftJoin('noseri_detail_pesanan', 'noseri_detail_pesanan.id', '=', 'uji_lab_detail.noseri_id')
+            ->leftJoin('t_gbj_noseri', 't_gbj_noseri.id', '=', 'noseri_detail_pesanan.t_tfbj_noseri_id')
+            ->leftJoin('noseri_barang_jadi', 'noseri_barang_jadi.id', '=', 't_gbj_noseri.noseri_id')
+            ->leftJoin('detail_metode_lab', 'detail_metode_lab.id', '=', 'uji_lab_detail.metode_id')
+            ->leftJoin('metode_lab', 'metode_lab.id', '=', 'detail_metode_lab.metode_lab_id')
+            ->leftJoin('erp_kesehatan.karyawans', 'karyawans.id', '=', 'uji_lab_detail.pemeriksa_id')
+            ->leftJoin('pesanan', 'pesanan.id', '=', 'uji_lab.pesanan_id')
+            ->leftJoin('jenis_pemilik', 'jenis_pemilik.id', '=', 'uji_lab.jenis_pemilik_id')
+            ->where('uji_lab_detail.status', '!=', 'belum');
 
-        $obj = [];
+        $spb = Spb::select('spb.pesanan_id as id', 'customer.nama', 'spb.ket')
+            ->selectRaw('"" AS no_paket')
+            ->selectRaw('"-" AS instansi')
+            ->selectRaw('"-" AS alamat_instansi')
+            ->selectRaw('"-" AS status')
+            ->selectRaw('"-" AS satuan')
+            ->selectRaw('"-" AS no_urut')
+            ->selectRaw('"-" AS tgl_buat')
+            ->selectRaw('"-" AS tgl_kontrak')
+            ->leftJoin('customer', 'customer.id', '=', 'spb.customer_id')
+            ->whereIn('spb.pesanan_id', $data->pluck('p_id')->toArray())->get();
 
-        foreach ($data as $d) {
-            $x = json_decode($d->response);
+        $spa = Spa::select('spa.pesanan_id as id', 'customer.nama', 'spa.ket')
+            ->selectRaw('"" AS no_paket')
+            ->selectRaw('"-" AS instansi')
+            ->selectRaw('"-" AS alamat_instansi')
+            ->selectRaw('"-" AS status')
+            ->selectRaw('"-" AS satuan')
+            ->selectRaw('"-" AS no_urut')
+            ->selectRaw('"-" AS tgl_buat')
+            ->selectRaw('"-" AS tgl_kontrak')
+            ->leftJoin('customer', 'customer.id', '=', 'spa.customer_id')
+            ->whereIn('spa.pesanan_id', $data->pluck('p_id')->toArray())->get();
+
+        $ekatalog = Ekatalog::select('ekatalog.pesanan_id as id', 'ekatalog.ket', 'ekatalog.tgl_buat', 'ekatalog.tgl_kontrak', 'ekatalog.no_urut as no_urut', 'customer.nama', 'ekatalog.no_paket', 'ekatalog.instansi', 'ekatalog.alamat as alamat_instansi', 'ekatalog.satuan', 'ekatalog.status')
+            ->leftJoin('customer', 'customer.id', '=', 'ekatalog.customer_id')
+            ->whereIn('ekatalog.pesanan_id', $data->pluck('p_id')->toArray())->get();
+
+        $dataInfo =   $ekatalog->merge($spa)->merge($spb);
+
+        foreach ($data->get() as $d) {
             $obj[] = array(
-                'id' => $d->id,
-                'order' => $x->no_order,
-                'nama' => $x->nama,
-                'jenis_pemilik' => $x->jenis_pemilik->label,
-                'so' => $x->so,
-                'customer' => $x->customer,
-                'tgl_kalibrasi' => $x->tgl_kalibrasi,
-                'jenis_transaksi' => 'internal',
-                'hasil' => $x->hasil,
-                'produk' => $x->produk
+                'no' => str_pad($d->no, 4, '0', STR_PAD_LEFT),
+                'p_id' => $d->p_id,
+                'no_po' => $d->no_po,
+                'no_order' => 'LAB-' . str_pad($d->no_order, 4, '0', STR_PAD_LEFT),
+                'tgl_masuk' => $d->tgl_masuk,
+                'nama_alat' => $d->metode,
+                'type' => $d->produk,
+                'noseri' => $d->noseri,
+                'nama_pemilik' => $d->jp,
+                'nama_pemilik_sert' => $d->nama,
+                'alamat' => $d->alamat,
+                'tgl_kalibrasi' => $d->tgl_kalibrasi,
+                'no_sertifikat' => $d->no_sertifikat,
+                'pemeriksa' => $d->pemeriksa,
+                // 'info' => $dataInfo->where('id',$d->p_id)->toArray(),
+                'nosj' => str_pad($d->no_order, 4, '0', STR_PAD_LEFT) . '/LAB/' . date('m', strtotime($d->tgl_kalibrasi)) . '/' . date('y', strtotime($d->tgl_kalibrasi)),
             );
         }
-        return response()->json($obj);
+        $merge = [];
+        foreach ($obj as $labItem) {
+            $mergedItem = $labItem;
+            $p_id = $labItem['p_id'];
+
+            foreach ($dataInfo as $poItem) {
+                if ($poItem['id'] === $p_id) {
+                    $mergedItem['info'] = $poItem;
+                    break;
+                }
+            }
+            $merge[] = $mergedItem;
+        }
+
+        return response()->json($merge);
+        // $years = $request->years;
+        // $data = SystemLog::where(['tipe' => 'Lab', 'subjek' => 'Kalibrasi Produk'])->whereYear('created_at', $years)->get();
+
+        // $obj = [];
+
+        // foreach ($data as $d) {
+        //     $x = json_decode($d->response);
+        //     $obj[] = array(
+        //         'id' => $d->id,
+        //         'order' => $x->no_order,
+        //         'nama' => $x->nama,
+        //         'jenis_pemilik' => $x->jenis_pemilik->label,
+        //         'so' => $x->so,
+        //         'customer' => $x->customer,
+        //         'tgl_kalibrasi' => $x->tgl_kalibrasi,
+        //         'jenis_transaksi' => 'internal',
+        //         'hasil' => $x->hasil,
+        //         'produk' => $x->produk
+        //     );
+        // }
+        // return response()->json($obj);
     }
 
     public function export_laporan(Request $request)
@@ -143,10 +238,7 @@ class LabController extends Controller
                 'nosj' => str_pad($d->no_order, 4, '0', STR_PAD_LEFT) . '/LAB/' . date('m', strtotime($d->tgl_kalibrasi)) . '/' . date('y', strtotime($d->tgl_kalibrasi)),
             );
         }
-
-
         $merge = [];
-
         foreach ($obj as $labItem) {
             $mergedItem = $labItem;
             $p_id = $labItem['p_id'];
@@ -157,10 +249,8 @@ class LabController extends Controller
                     break;
                 }
             }
-
             $merge[] = $mergedItem;
         }
-
         return response()->json($merge);
     }
     public function bom_detail($id)
