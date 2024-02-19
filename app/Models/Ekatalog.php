a41e78bf955ab7d0ae506a2f0669abf7883816f2
dimaseka83 <dimaseka83@gmail.com>
2024-02-19 08:44:41 +0700

    ==

--- app/Models/Ekatalog.php
+++ app/Models/Ekatalog.php
@@ -10,7 +10,6 @@ class Ekatalog extends Model
     protected $connection = 'erp';
     protected $table = 'ekatalog';
     protected $fillable = ['created_at','updated_at','customer_id', 'provinsi_id', 'pesanan_id', 'no_paket', 'no_urut', 'deskripsi', 'instansi', 'alamat', 'satuan', 'status', 'tgl_kontrak', 'tgl_buat', 'tgl_edit', 'ket', 'log'];
-    protected $fillable = ['created_at', 'updated_at', 'customer_id', 'provinsi_id', 'pesanan_id', 'no_paket', 'no_urut', 'deskripsi', 'instansi', 'alamat', 'satuan', 'status', 'tgl_kontrak', 'tgl_buat', 'tgl_edit', 'ket', 'log'];
 
     public function Customer()
     {
