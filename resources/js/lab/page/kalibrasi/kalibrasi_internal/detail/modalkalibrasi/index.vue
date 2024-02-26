15bd664737a0e70b074eb302590325ca8390505e
dimaseka83 <dimaseka83@gmail.com>
2024-02-26 08:46:08 +0700

    ++

--- resources/js/lab/page/kalibrasi/kalibrasi_internal/detail/modalkalibrasi/index.vue
+++ resources/js/lab/page/kalibrasi/kalibrasi_internal/detail/modalkalibrasi/index.vue
@@ -74,7 +74,7 @@ export default {
     <div class="modal fade modalKalibrasi" id="exampleModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
         aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-scrollable">
-            <div class="modal-content" style="overflow: hidden;">
+            <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">
                         Form Kalibrasi
