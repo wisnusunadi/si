a12b2dda9b439d8d5693ae48882cafc9eb6fbf52
dimaseka83 <dimaseka83@gmail.com>
2024-03-18 11:32:47 +0700

    +++

---
+++ resources/js/ppic_spa/views/SalesOrder/detailProdukSO.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: ['showModal'],
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    },
+}
+</script>
+<template>
+    <div class="modal" :class="{'is-active': showModal}">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <div class="modal-card-head">
+                <p class="moda-card-title"></p>
+                <button class="delete" @click="show"></button>
+            </div>
+        </div>
+    </div>
+</template>
