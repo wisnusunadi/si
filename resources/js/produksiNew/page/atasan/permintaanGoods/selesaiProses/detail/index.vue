674a88780f7aefe45ae6451e27d98226d52b1331
dimaseka83 <dimaseka83@gmail.com>
2024-04-17 16:09:40 +0700

    ++

---
+++ resources/js/produksiNew/page/atasan/permintaanGoods/selesaiProses/detail/index.vue
@@ -0,0 +1,30 @@
+<script>
+import Header from '../../../../../components/header.vue';
+export default {
+    components: { Header },
+    data() {
+        return {
+            title: 'Detail Permintaan Barang',
+            breadcumbs: [
+                {
+                    name: 'Home',
+                    link: '/'
+                },
+                {
+                    name: 'Permintaan Barang',
+                    link: '/produksi/permintaan_goods'
+                },
+                {
+                    name: 'Detail Permintaan Barang',
+                    link: '/permintaan-goods/detail'
+                }
+            ]
+        }
+    },
+}
+</script>
+<template>
+    <div>
+        <Header :title="title" :breadcumbs="breadcumbs" />
+    </div>
+</template>
