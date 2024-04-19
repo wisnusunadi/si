790c7dd42cedec28e35bad2cc58da56ffeb1b338
dimaseka83 <dimaseka83@gmail.com>
2024-04-19 11:06:01 +0700

    add edit

---
+++ resources/js/produksiNew/page/permintaanGoods/daftarProses/detail.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    methods: {
+        closeModal() {
+            $('.modalDetail').modal('hide');
+            this.$nextTick(() => {
+                this.$emit('close');
+            });
+        }
+    },
+}
+</script>
+<template>
+    <div class="modal fade modalDetail" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+        aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" @click="closeModal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Body
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
