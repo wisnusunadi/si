2541c96a22eb3aa235375f8e7633109645666c03
dimaseka83 <dimaseka83@gmail.com>
2024-07-04 08:39:49 +0700

    ++

--- resources/js/meeting/page/meeting/detailterlaksana/notulen/index.vue
+++ resources/js/meeting/page/meeting/detailterlaksana/notulen/index.vue
@@ -89,57 +89,72 @@ export default {
 </script>
 <template>
     <div class="card">
-        <modal @refresh="$emit('refresh')" v-if="showModal" @closeModal="close" />
+        <modal
+            @refresh="$emit('refresh')"
+            v-if="showModal"
+            @closeModal="close"
+        />
         <div class="card-body">
             <div class="d-flex">
                 <div class="mr-auto p-2">
-                    <button class="btn btn-primary" @click="addNotulen" v-if="status == 'menyusun_hasil_meeting'">
+                    <button
+                        class="btn btn-primary"
+                        @click="addNotulen"
+                        v-if="status == 'menyusun_hasil_meeting'"
+                    >
                         <i class="fa fa-plus"></i>
                         Tambah
                     </button>
                 </div>
                 <div class="p-2" v-if="status != 'menyusun_hasil_meeting'">
-                    <input type="text" class="form-control" placeholder="Cari..." v-model="search" />
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Cari..."
+                        v-model="search"
+                    />
                 </div>
             </div>
-            <table class="table">
-                <thead class="text-center">
-                    <tr>
-                        <th rowspan="2">No</th>
-                        <th rowspan="2">Penanggung Jawab</th>
-                        <th rowspan="2" style="width: 600px;">Uraian</th>
-                        <th colspan="2">Kesesuian</th>
-                        <th rowspan="2" style="width: 500px;">Catatan</th>
-                    </tr>
-                    <tr>
-                        <th>Hasil</th>
-                        <th>Dicek Oleh</th>
-                    </tr>
-                </thead>
-                <tbody class="tbodyDragNDrop text-center" v-if="status == 'menyusun_hasil_meeting'">
-                    <tr v-for="(item, idx) in meeting" :key="idx">
-                        <td>{{ idx + 1 }}</td>
-                        <td>
-                            {{ item.pic }} - {{ item.jabatan }}
-                        </td>
-                        <td class="text-justify">{{ item.isi }}</td>
-                        <td>
-                            <kehadiran :kehadiran="item.hasil" />
-                        </td>
-                        <td>
-                            <div v-if="item.dicek">
-                                <span class="text-capitalize">{{ changeUnderscoreToSpace(item.hasil) }}</span>, Oleh
-                                {{ item.dicek }},
-                                {{ item.checked_at }}
-                            </div>
-                            <div v-else>
-                                -
-                            </div>
-                        </td>
-                        <td class="text-justify">
-                            {{ item.catatan ?? "-" }}
-                        </td>
-                        <!-- <td>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="text-center">
+                        <tr>
+                            <th rowspan="2">No</th>
+                            <th rowspan="2">Penanggung Jawab</th>
+                            <th rowspan="2" style="width: 600px">Uraian</th>
+                            <th colspan="2">Kesesuian</th>
+                            <th rowspan="2" style="width: 500px">Catatan</th>
+                        </tr>
+                        <tr>
+                            <th>Hasil</th>
+                            <th>Dicek Oleh</th>
+                        </tr>
+                    </thead>
+                    <tbody
+                        class="tbodyDragNDrop text-center"
+                        v-if="status == 'menyusun_hasil_meeting'"
+                    >
+                        <tr v-for="(item, idx) in meeting" :key="idx">
+                            <td>{{ idx + 1 }}</td>
+                            <td>{{ item.pic }} - {{ item.jabatan }}</td>
+                            <td class="text-justify text-wrap">{{ item.isi }}</td>
+                            <td>
+                                <kehadiran :kehadiran="item.hasil" />
+                            </td>
+                            <td>
+                                <div v-if="item.dicek">
+                                    <span class="text-capitalize">{{
+                                        changeUnderscoreToSpace(item.hasil)
+                                    }}</span
+                                    >, Oleh {{ item.dicek }},
+                                    {{ item.checked_at }}
+                                </div>
+                                <div v-else>-</div>
+                            </td>
+                            <td class="text-justify text-wrap">
+                                {{ item.catatan ?? "-" }}
+                            </td>
+                            <!-- <td>
                             <button class="btn btn-outline-warning" @click="editNotulen(item, idx)">
                                 <i class="fa fa-edit"></i>
                             </button>
@@ -147,36 +162,42 @@ export default {
                                 <i class="fa fa-trash"></i>
                             </button>
                         </td> -->
-                    </tr>
-                </tbody>
-                <tbody v-if="status != 'menyusun_hasil_meeting'">
-                    <tr v-for="(item, idx) in renderPaginate" :key="idx" class="text-center">
-                        <td>{{ idx + 1 }}</td>
-                        <td>
-                            {{ item.pic }} - {{ item.jabatan }}
-                        </td>
-                        <td class="text-justify">{{ item.isi }}</td>
-                        <td>
-                            <kehadiran :kehadiran="item.hasil" />
-                        </td>
-                        <td>
-                            <div v-if="item.dicek">
-                                <span class="text-capitalize">{{ changeUnderscoreToSpace(item.hasil) }}</span>, Oleh
-                                {{ item.dicek }},
-                                {{ item.checked_at }}
-                            </div>
-                            <div v-else>
-                                -
-                            </div>
-                        </td>
-                        <td class="text-justify">
-                            {{ item.catatan ?? "-" }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <pagination :filteredDalamProses="paginateData" @updateFilteredDalamProses="updatePage"
-                v-if="status != 'menyusun_hasil_meeting'" />
+                        </tr>
+                    </tbody>
+                    <tbody v-if="status != 'menyusun_hasil_meeting'">
+                        <tr
+                            v-for="(item, idx) in renderPaginate"
+                            :key="idx"
+                            class="text-center"
+                        >
+                            <td>{{ idx + 1 }}</td>
+                            <td>{{ item.pic }} - {{ item.jabatan }}</td>
+                            <td class="text-justify">{{ item.isi }}</td>
+                            <td>
+                                <kehadiran :kehadiran="item.hasil" />
+                            </td>
+                            <td>
+                                <div v-if="item.dicek">
+                                    <span class="text-capitalize">{{
+                                        changeUnderscoreToSpace(item.hasil)
+                                    }}</span
+                                    >, Oleh {{ item.dicek }},
+                                    {{ item.checked_at }}
+                                </div>
+                                <div v-else>-</div>
+                            </td>
+                            <td class="text-justify">
+                                {{ item.catatan ?? "-" }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <pagination
+                :filteredDalamProses="paginateData"
+                @updateFilteredDalamProses="updatePage"
+                v-if="status != 'menyusun_hasil_meeting'"
+            />
         </div>
     </div>
 </template>
