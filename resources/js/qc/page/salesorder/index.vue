acc1c71f1f5825612d72cb7dfd27e5700ec0ed8c
dimaseka83 <dimaseka83@gmail.com>
2024-07-11 11:23:26 +0700

    add badge notification on batal po qc, log

--- resources/js/qc/page/salesorder/index.vue
+++ resources/js/qc/page/salesorder/index.vue
@@ -1,114 +1,147 @@
 <script>
-import Header from '../../components/header.vue'
-import dalamproses from './dalamproses'
-import selesaiproses from './selesaiproses'
-import batalpo from './batalpo'
-import axios from 'axios'
+import Header from "../../components/header.vue";
+import dalamproses from "./dalamproses";
+import selesaiproses from "./selesaiproses";
+import batalpo from "./batalpo";
+import axios from "axios";
 export default {
     components: {
         Header,
         dalamproses,
         selesaiproses,
-        batalpo
+        batalpo,
     },
     data() {
         return {
-            title: 'Sales Order',
+            title: "Sales Order",
             breadcumbs: [
                 {
-                    name: 'Beranda',
-                    link: '/'
+                    name: "Beranda",
+                    link: "/",
                 },
                 {
-                    name: 'Sales Order Qc',
-                    link: '/salesorder'
-                }
+                    name: "Sales Order Qc",
+                    link: "/salesorder",
+                },
             ],
             dalamProsesData: [],
             selesaiProsesData: [],
             batalPoData: [],
-            jenisDalamProsesStatus: ['semua'],
-            jenisSelesaiProsesStatus: ['semua'],
-        }
+            jenisDalamProsesStatus: ["semua"],
+            jenisSelesaiProsesStatus: ["semua"],
+            isBatalPo: false,
+        };
     },
     methods: {
         async getData() {
             try {
-                this.$store.dispatch('setLoading', true)
-                const { data: dalamproses } = await axios.post(`/api/qc/so/data/${this.jenisDalamProsesStatus}`, {}, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('lokal_token')}`
+                this.$store.dispatch("setLoading", true);
+                const { data: dalamproses } = await axios.post(
+                    `/api/qc/so/data/${this.jenisDalamProsesStatus}`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                "lokal_token"
+                            )}`,
+                        },
                     }
-                })
+                );
                 this.dalamProsesData = dalamproses.map((item, index) => {
                     return {
                         no: index + 1,
                         ...item,
-                    }
-                })
+                    };
+                });
 
-                const { data: selesai } = await axios.post(`/api/qc/so/data/selesai/${this.jenisSelesaiProsesStatus}`, {}, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('lokal_token')}`
+                const { data: selesai } = await axios.post(
+                    `/api/qc/so/data/selesai/${this.jenisSelesaiProsesStatus}`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                "lokal_token"
+                            )}`,
+                        },
                     }
-                })
+                );
                 this.selesaiProsesData = selesai.map((item, index) => {
                     return {
                         no: index + 1,
                         ...item,
-                    }
-                })
+                    };
+                });
 
-                const { data: batalPO } = await axios.get('/api/penjualan/batal_po/qc/show')
+                const { data: batalPO } = await axios.get(
+                    "/api/penjualan/batal_po/qc/show"
+                );
                 this.batalPoData = batalPO.map((item, index) => {
                     return {
                         no: index + 1,
                         ...item,
-                    }
-                })
+                    };
+                });
+
+                if (this.batalPoData.length > 0) {
+                    this.isBatalPo = true;
+                } else {
+                    this.isBatalPo = false;
+                }
             } catch (error) {
-                console.log(error)
+                console.log(error);
             } finally {
-                this.$store.dispatch('setLoading', false)
+                this.$store.dispatch("setLoading", false);
             }
         },
         filterDataStatusDalamProses(status) {
             if (this.jenisDalamProsesStatus.includes(status)) {
-                this.jenisDalamProsesStatus = this.jenisDalamProsesStatus.filter(item => item !== status)
+                this.jenisDalamProsesStatus =
+                    this.jenisDalamProsesStatus.filter(
+                        (item) => item !== status
+                    );
             } else {
-                this.jenisDalamProsesStatus = this.jenisDalamProsesStatus.filter(item => item !== 'semua')
-                this.jenisDalamProsesStatus.push(status)
+                this.jenisDalamProsesStatus =
+                    this.jenisDalamProsesStatus.filter(
+                        (item) => item !== "semua"
+                    );
+                this.jenisDalamProsesStatus.push(status);
             }
 
             if (this.jenisDalamProsesStatus.length === 0) {
-                this.jenisDalamProsesStatus.push('semua')
+                this.jenisDalamProsesStatus.push("semua");
             }
 
             this.$nextTick(() => {
-                this.getData()
-            })
+                this.getData();
+            });
         },
         filterDataStatusSelesaiProses(status) {
             if (this.jenisSelesaiProsesStatus.includes(status)) {
-                this.jenisSelesaiProsesStatus = this.jenisSelesaiProsesStatus.filter(item => item !== status)
+                this.jenisSelesaiProsesStatus =
+                    this.jenisSelesaiProsesStatus.filter(
+                        (item) => item !== status
+                    );
             } else {
-                this.jenisSelesaiProsesStatus = this.jenisSelesaiProsesStatus.filter(item => item !== 'semua')
-                this.jenisSelesaiProsesStatus.push(status)
+                this.jenisSelesaiProsesStatus =
+                    this.jenisSelesaiProsesStatus.filter(
+                        (item) => item !== "semua"
+                    );
+                this.jenisSelesaiProsesStatus.push(status);
             }
 
             if (this.jenisSelesaiProsesStatus.length === 0) {
-                this.jenisSelesaiProsesStatus.push('semua')
+                this.jenisSelesaiProsesStatus.push("semua");
             }
 
             this.$nextTick(() => {
-                this.getData()
-            })
-        }
+                this.getData();
+            });
+        },
     },
     created() {
-        this.getData()
-    }
-}
+        this.getData();
+    },
+};
 </script>
 <template>
     <div class="font-medium">
@@ -117,30 +150,80 @@ export default {
             <div class="card-body" v-if="!$store.state.loading">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home"
-                            type="button" role="tab" aria-controls="pills-home" aria-selected="true">Dalam
-                            Proses</a>
+                        <a
+                            class="nav-link active"
+                            id="pills-home-tab"
+                            data-toggle="pill"
+                            data-target="#pills-home"
+                            type="button"
+                            role="tab"
+                            aria-controls="pills-home"
+                            aria-selected="true"
+                            >Dalam Proses</a
+                        >
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile"
-                            type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Selesai
-                            Proses</a>
+                        <a
+                            class="nav-link"
+                            id="pills-profile-tab"
+                            data-toggle="pill"
+                            data-target="#pills-profile"
+                            type="button"
+                            role="tab"
+                            aria-controls="pills-profile"
+                            aria-selected="false"
+                            >Selesai Proses</a
+                        >
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact"
-                            type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Batal
-                            PO</a>
+                        <a
+                            class="nav-link"
+                            id="pills-contact-tab"
+                            data-toggle="pill"
+                            data-target="#pills-contact"
+                            type="button"
+                            role="tab"
+                            aria-controls="pills-contact"
+                            aria-selected="false"
+                            >Batal PO
+                            <span class="badge badge-danger" v-if="isBatalPo">
+                                <span>
+                                    Ada PO yang dibatalkan
+                                </span>
+                            </span>
+                        </a>
                     </li>
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                        aria-labelledby="pills-home-tab">
-                        <dalamproses :dalam="dalamProsesData" @filter="filterDataStatusDalamProses" @refresh="getData" />
+                    <div
+                        class="tab-pane fade show active"
+                        id="pills-home"
+                        role="tabpanel"
+                        aria-labelledby="pills-home-tab"
+                    >
+                        <dalamproses
+                            :dalam="dalamProsesData"
+                            @filter="filterDataStatusDalamProses"
+                            @refresh="getData"
+                        />
                     </div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                        <selesaiproses :selesai="selesaiProsesData" @filter="filterDataStatusSelesaiProses" />
+                    <div
+                        class="tab-pane fade"
+                        id="pills-profile"
+                        role="tabpanel"
+                        aria-labelledby="pills-profile-tab"
+                    >
+                        <selesaiproses
+                            :selesai="selesaiProsesData"
+                            @filter="filterDataStatusSelesaiProses"
+                        />
                     </div>
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <div
+                        class="tab-pane fade"
+                        id="pills-contact"
+                        role="tabpanel"
+                        aria-labelledby="pills-contact-tab"
+                    >
                         <batalpo :items="batalPoData" @refresh="getData" />
                     </div>
                 </div>
